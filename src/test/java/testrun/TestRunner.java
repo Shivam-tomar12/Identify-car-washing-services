dc4d6216def17f24a524a20de92ee6fa51ed44d8
shivam <12shivamt@gmail.com>
2024-03-01 21:39:33 +0530

    Fourth

--- src/test/java/testrun/TestRunner.java
+++ src/test/java/testrun/TestRunner.java
@@ -3,11 +3,11 @@ package testrun;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
  
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features= {".//feature/user.feature"},
 		glue="Stepdefi",
 		plugin= {"pretty","html:reports/myreport.html", "rerun:target/reurn.txt","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
